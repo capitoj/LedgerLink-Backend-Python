0792a62fda50c568ebf86df41d0755d74d4e3a63
aonomike <aonomike@gmail.com>
2018-03-06 20:12:34 +0300

    Removed Added Space in base.py

--- mainsite/settings/base.py
+++ mainsite/settings/base.py
@@ -119,7 +119,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, '../db.sqlite3'),
-
     },
 #    'some_my_sql': {
 #        'NAME': 'mysql_db',
