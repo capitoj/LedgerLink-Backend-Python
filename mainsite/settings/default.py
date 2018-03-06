a021a705263bc99024f24b4e1ee0b4f89f7e607d
aonomike <aonomike@gmail.com>
2018-03-06 16:50:55 +0300

    Added default.sqlite3
    - Added default.sqlite3 database
    - Added a settings file mainsite/settings/default.py in the database

---
+++ mainsite/settings/default.py
@@ -0,0 +1,13 @@
+from .base import *
+
+DEBUG=True
+
+#ALLOWED_HOSTS = ['localhost','127.0.0.1','ptbi-dashboard.globalhealthapp.net']
+ALLOWED_HOSTS = ['*']
+#USE_X_FORWARDED_HOST = True
+
+DATABASES = {
+    'default': {
+        'NAME': os.path.join(BASE_DIR, '../default.sqlite3'),
+    },
+}
