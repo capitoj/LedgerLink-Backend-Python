b3da50491c0ee6d82747c8bdab8ad36f9f143c94
walleza <jessen.sengulane@example.com>
2018-05-24 13:45:27 +0200

    Adding STATIC_ROOT and removing _

--- mainsite/settings/dev.py
+++ mainsite/settings/dev.py
@@ -4,4 +4,4 @@ from .base import *
 ALLOWED_HOSTS = ['*']
 DEBUG = True
 
-STATIC_ROOT = '/var/www/xf_sample.globalhealthapp.net/static/'
+STATIC_ROOT = '/var/www/xf-sample.globalhealthapp.net/static/'
